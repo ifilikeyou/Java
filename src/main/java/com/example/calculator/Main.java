eec9a2a8fae03110dbdc2efa6e23c78c03add884
朱杰 <15801635316@163.com>
2026-01-08 09:11:25 +0800

    修改错误

--- src/main/java/com/example/calculator/Main.java
+++ src/main/java/com/example/calculator/Main.java
@@ -3,7 +3,7 @@ package com.example.calculator;
 /**
  * 计算器程序主类
  */
-public class Main {1
+public class Main {
     public static void main(String[] args) {
         // 启动计算器用户界面
         CalculatorUI calculatorUI = new CalculatorUI();
